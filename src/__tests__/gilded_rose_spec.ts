8c70c2ed5b06642161da14cdd8f1420e7dcf028a
zvukarci <149152199+zvukarci@users.noreply.github.com>
2025-09-09 12:04:20 +0200

    unit tests added

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -1,11 +1,47 @@
-import { Shop, Item } from '../gilded_rose';
+import { Shop, Item } from "../gilded_rose";
 
 describe("Gilded Rose", () => {
+    it("quality of Sulfuras must be equal 80", () => {
+        const gildedRose = new Shop([
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        ]);
 
-    it("should foo", () => {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+
+        expect(items[0].quality).toEqual(80);
+    });
+});
+
+describe("regular item", () => {
+    it("quality decreases at 2x speed", () => {
+        const gildedRose = new Shop([new Item("Kreatinos Podperos", -1, 2)]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(0);
     });
 
+    it("never negative", () => {
+        const gildedRose = new Shop([new Item("Kreatinos Podperos", -1, 0)]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(0);
+    });
+
+    it("quality maximum is 50", () => {
+        const gildedRose = new Shop([new Item("Kreatinos Podperos", -1, 50)]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBeLessThanOrEqual(50);
+    });
+
+    it("quality decreases", () => {
+        const gildedRose = new Shop([new Item("Kreatinos Podperos", 10, 1)]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toEqual(0);
+    });
 });
